29141ea80004b7ec5800b88c979523add2f3e9df
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2026-04-08 21:38:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,21 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[todo, setTodo] = useState([
+    {id: 1, text: 'Learn React', completed: false},
+    {id: 2, text: 'Build a React app', completed: false},
+    {id: 3, text: 'Deploy the React app', completed: false}
+  ]);
+  const handleComplete=(id)=>{
+    const updateTodos = todos.map(todo=> 
+      todo.id === id ? {...too, completed: true} : todo
+    );
+    setTodo(updateTodos); 
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
   )
 }
